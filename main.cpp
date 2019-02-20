40261c70e6e593d8ed284defae7f474b7eeb97ed
Mario Mejia <mariomejia@wlan-t-83-170-dhcp.CSUChico.EDU>
2019-02-20 11:30:51 -0800

    updated the main for the given bug

--- main.cpp
+++ main.cpp
@@ -12,28 +12,29 @@ int main()
   cout<<"Enter any three numbers: ";
   cin>>numA>>numB>>numC;
 
-  sortDescending(numA, numB, numC);
   
   cout<<"From greatest to least, they are: ";
-  cout<<numA<<","<<numB<<","<<numC<<endl;
+  sortDescending(numA, numB, numC);
   return 0;
 }
+/*Our main function prompts the user for three values to sort outputing them correctly in descending order.*/
 
 void sortDescending(int first, int second, int third)
 {
   if( first < third )
   {
-    swap(first,third);
+    swap(third,first);
   }
   if( first < second )
   {
-    swap(first,second);
+    swap(second,first);
   }
   if( second < third )
   {
-    swap(second,third);
+    swap(third,second);
   }
-}
+  cout<<first<<","<<second<<","<<third<<endl;
+}/*Our sort orders our values by swaping variable names and values correctly using the given condintons within our if statements.Outputing the updated variables.*/
 
 void swap(int &first, int &second)
 {
@@ -41,3 +42,4 @@ void swap(int &first, int &second)
   first = second;
   second = temp;
 }
+/*Our swap takes in the address to our value and swaps it the other address that is given within the parameters.*/
